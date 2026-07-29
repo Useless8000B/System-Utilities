fac6c5ca0f116840a528159db25d0cbf8823b037
Useless8000B <adrielluiz73@gmail.com>
2026-07-29 09:50:15 -0300

    Add new option

--- src/view/menu.rs
+++ src/view/menu.rs
@@ -1,13 +1,15 @@
 pub enum Menu {
 	ZRAM,
 	RAM,
+	VRAM,
 }
 
 impl Menu {
 	pub fn as_string(&self) -> &str {
 		match self {
 			Menu::ZRAM => "[1] Check ZRAM usage",
-			Menu::RAM => "[2] Check RAM usage"
+			Menu::RAM => "[2] Check RAM usage",
+			Menu::VRAM => "[3] Check VRAM usage"
 		}
 	}
 
@@ -15,11 +17,12 @@ impl Menu {
 		match choice {
 			1 => Some(Menu::ZRAM),
 			2 => Some(Menu::RAM),
+			3 => Some(Menu::VRAM),
 			_ => None			
 		}
 	}
 
-	pub fn all() -> [Menu; 2] {
-		[Menu::ZRAM, Menu::RAM]
+	pub fn all() -> [Menu; 3] {
+		[Menu::ZRAM, Menu::RAM, Menu::VRAM]
 	}
 }
