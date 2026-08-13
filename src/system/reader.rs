fc9f037bdc85f506b6a790c8012f22fc1bae89c4
Useless8000B <adrielluiz73@gmail.com>
2026-08-13 19:18:32 -0300

    Update imports

--- src/system/reader.rs
+++ src/system/reader.rs
@@ -5,13 +5,13 @@ use std::time::Duration;
 use sysinfo::System;
 use sysinfo::Disks;
 
-use crate::models::memory_model::MemoryModel;
-use crate::models::pu_model::PuModel;
-use crate::models::storage_model::StorageModel;
+use crate::models::memory::Memory;
+use crate::models::pu::Pu;
+use crate::models::storage::Storage;
 use crate::system::sensors::Sensor;
 use crate::utils::extract_from_label::extract_from_label;
 
-pub fn read_ram_info() -> Result<MemoryModel, String> {
+pub fn read_ram_info() -> Result<Memory, String> {
     let sensors = Sensor::memory_sensors();
 
     let raw_ram = sensors
@@ -22,13 +22,13 @@ pub fn read_ram_info() -> Result<MemoryModel, String> {
     let raw_total_ram = extract_from_label(&raw_ram.path, "MemTotal")?;
     let raw_available_ram = extract_from_label(&raw_ram.path, "MemAvailable")?;
 
-    Ok(MemoryModel {
+    Ok(Memory {
         total: raw_total_ram as f64 / (1024.0 * 1024.0),
         used: (raw_total_ram as f64 - raw_available_ram as f64) / (1024.0 * 1024.0),
     })
 }
 
-pub fn read_zram_info() -> Result<MemoryModel, String> {
+pub fn read_zram_info() -> Result<Memory, String> {
     let sensors = Sensor::memory_sensors();
 
     let raw_zram = sensors
@@ -39,13 +39,13 @@ pub fn read_zram_info() -> Result<MemoryModel, String> {
     let raw_total_zram = extract_from_label(&raw_zram.path, "SwapTotal")?;
     let raw_free_zram = extract_from_label(&raw_zram.path, "SwapFree")?;
 
-    Ok(MemoryModel {
+    Ok(Memory {
         total: raw_total_zram as f64 / (1024.0 * 1024.0),
         used: (raw_total_zram - raw_free_zram) as f64 / (1024.0 * 1024.0),
     })
 }
 
-pub fn read_vram_info() -> Result<MemoryModel, String> {
+pub fn read_vram_info() -> Result<Memory, String> {
     let sensors = Sensor::memory_sensors();
 
     let raw_total_vram = sensors
@@ -60,13 +60,13 @@ pub fn read_vram_info() -> Result<MemoryModel, String> {
         .ok_or("VRAM_USED sensor not found!")?
         .read_sensor()?;
 
-    Ok(MemoryModel {
+    Ok(Memory {
         total: raw_total_vram as f64 / (1024.0 * 1024.0),
         used: raw_used_vram as f64 / (1024.0 * 1024.0),
     })
 }
 
-pub fn read_cpu_info() -> Result<PuModel, String> {
+pub fn read_cpu_info() -> Result<Pu, String> {
     let sensors = Sensor::cpu_sensors();
     let mut sys = System::new_all();
 
@@ -81,13 +81,13 @@ pub fn read_cpu_info() -> Result<PuModel, String> {
         .ok_or("INTEL_AVERAGE_TEMPERATURE sensor not found!")?
         .read_sensor()?;
 
-    Ok(PuModel {
+    Ok(Pu {
         temperature: raw_intel_average_temperature as f32 / 1000.0,
         usage: cpu_usage,
     })
 }
 
-pub fn read_gpu_info() -> Result<PuModel, String> {
+pub fn read_gpu_info() -> Result<Pu, String> {
     let sensors = Sensor::gpu_sensors();
 
     let raw_amd_gpu_temperature = sensors
@@ -102,13 +102,13 @@ pub fn read_gpu_info() -> Result<PuModel, String> {
         .ok_or("AMD_GPU_USAGE sensor not found!")?
         .read_sensor()?;
 
-    Ok(PuModel {
+    Ok(Pu {
         temperature: raw_amd_gpu_temperature as f32 / 1000.0,
         usage: raw_amd_gpu_usage as u8,
     })
 }
 
-pub fn read_storage_info() -> Result<StorageModel, String> {
+pub fn read_storage_info() -> Result<Storage, String> {
     let sensors = Sensor::storage_sensors();
     let disks = Disks::new_with_refreshed_list();
     let target = Path::new("/");
@@ -129,7 +129,7 @@ pub fn read_storage_info() -> Result<StorageModel, String> {
         .ok_or("NVME_SIZE sensor not found!")?
         .read_sensor()?;
 
-    Ok(StorageModel {
+    Ok(Storage {
         temperature: raw_storage_temperature as f32 / 1000.0,
         used: disk.available_space() as f64 / (1024.0 * 1024.0 * 1024.0),
         total: (raw_storage_size * 512) as f64 / (1024.0 * 1024.0 * 1024.0),
