d989a677103190ef02bd760efda92ed12f6ddb55
Useless8000B <adrielluiz73@gmail.com>
2026-08-25 19:47:23 -0300

    Implement new error enum

--- src/utils/extract_from_label.rs
+++ src/utils/extract_from_label.rs
@@ -1,9 +1,10 @@
 use std::fs;
+use crate::errors::reader_error::ReaderError;
 
-pub fn extract_from_label(path: &str, label: &str) -> Result<u64, String> {
+pub fn extract_from_label(path: &str, label: &str) -> Result<u64, ReaderError> {
     let content =
         fs::read_to_string(path)
-            .map_err(|e| format!("Error reading file {path}: {e}"))?;
+            .map_err(|e| ReaderError::ReadingError(format!("Error reading {label} at path {path}: {e}")))?;
 
     let label_value = content
         .lines()
@@ -16,7 +17,7 @@ pub fn extract_from_label(path: &str, label: &str) -> Result<u64, String> {
                 None
             }
         })
-        .ok_or_else(|| format!("Error reading {label}"))?;
+        .ok_or_else(|| ReaderError::ReadingError(format!("Error reading {label}")))?;
 
     Ok(label_value)
 }
