07a71a69fa322e366b93859d0836ce0cea816288
Useless8000B <adrielluiz73@gmail.com>
2026-08-25 19:49:37 -0300

    Refactor
    - Implement new error enum
    
    - Implement find_sensor method instead of manual loop

--- src/system/reader.rs
+++ src/system/reader.rs
@@ -10,17 +10,13 @@ use crate::models::pu::Pu;
 use crate::models::storage::Storage;
 use crate::system::sensors::Sensor;
 use crate::utils::extract_from_label::extract_from_label;
+use crate::errors::reader_error::ReaderError;
 
 const MILLIDEGREES_C_TO_CELSIUS: f64 = 1000.0;
 
-pub fn read_ram_info() -> Result<Memory, String> {
+pub fn read_ram_info() -> Result<Memory, ReaderError> {
     let sensors = Sensor::memory_sensors();
-
-    let raw_ram = sensors
-        .iter()
-        .find(|v| v.name == "RAM")
-        .ok_or("RAM sensor not found!")?;
-
+    let raw_ram = Sensor::find_sensor(sensors, "RAM")?;
     let raw_total_ram = extract_from_label(raw_ram.path, "MemTotal")?;
     let raw_available_ram = extract_from_label(raw_ram.path, "MemAvailable")?;
 
@@ -30,14 +26,9 @@ pub fn read_ram_info() -> Result<Memory, String> {
     })
 }
 
-pub fn read_zram_info() -> Result<Memory, String> {
+pub fn read_zram_info() -> Result<Memory, ReaderError> {
     let sensors = Sensor::memory_sensors();
-
-    let raw_zram = sensors
-        .iter()
-        .find(|v| v.name == "ZRAM")
-        .ok_or("ZRAM sensor not found!")?;
-
+    let raw_zram = Sensor::find_sensor(sensors, "ZRAM")?;
     let raw_total_zram = extract_from_label(raw_zram.path, "SwapTotal")?;
     let raw_free_zram = extract_from_label(raw_zram.path, "SwapFree")?;
 
@@ -47,7 +38,7 @@ pub fn read_zram_info() -> Result<Memory, String> {
     })
 }
 
-pub fn read_vram_info() -> Result<Memory, String> {
+pub fn read_vram_info() -> Result<Memory, ReaderError> {
     let sensors = Sensor::memory_sensors();
     let raw_total_vram = Sensor::find_sensor(sensors, "VRAM_TOTAL")?.read_sensor()?;
     let raw_used_vram = Sensor::find_sensor(sensors, "VRAM_USED")?.read_sensor()?;
@@ -58,7 +49,7 @@ pub fn read_vram_info() -> Result<Memory, String> {
     })
 }
 
-pub fn read_cpu_info() -> Result<Pu, String> {
+pub fn read_cpu_info() -> Result<Pu, ReaderError> {
     let sensors = Sensor::cpu_sensors();
     let mut sys = System::new_all();
 
@@ -67,10 +58,7 @@ pub fn read_cpu_info() -> Result<Pu, String> {
     sys.refresh_cpu_usage();
 
     let cpu_usage: u8 = sys.global_cpu_usage() as u8;
-    let raw_intel_average_temperature = sensors
-        .iter()
-        .find(|v| v.name == "INTEL_AVERAGE_TEMPERATURE")
-        .ok_or("INTEL_AVERAGE_TEMPERATURE sensor not found!")?
+    let raw_intel_average_temperature = Sensor::find_sensor(sensors, "INTEL_AVERAGE_TEMPERATURE")?
         .read_sensor()?;
 
     Ok(Pu {
@@ -79,20 +67,11 @@ pub fn read_cpu_info() -> Result<Pu, String> {
     })
 }
 
-pub fn read_gpu_info() -> Result<Pu, String> {
+pub fn read_gpu_info() -> Result<Pu, ReaderError> {
     let sensors = Sensor::gpu_sensors();
-
-    let raw_amd_gpu_temperature = sensors
-        .iter()
-        .find(|v| v.name == "AMD_GPU_TEMPERATURE")
-        .ok_or("AMD_GPU_TEMPERATURE sensor not found!")?
+    let raw_amd_gpu_temperature = Sensor::find_sensor(sensors, "AMD_GPU_TEMPERATURE")?
         .read_sensor()?;
-
-    let raw_amd_gpu_usage = sensors
-        .iter()
-        .find(|v| v.name == "AMD_GPU_USAGE")
-        .ok_or("AMD_GPU_USAGE sensor not found!")?
-        .read_sensor()?;
+    let raw_amd_gpu_usage = Sensor::find_sensor(sensors, "AMD_GPU_USAGE")?.read_sensor()?;
 
     Ok(Pu {
         temperature: raw_amd_gpu_temperature as f64 / MILLIDEGREES_C_TO_CELSIUS,
@@ -100,17 +79,16 @@ pub fn read_gpu_info() -> Result<Pu, String> {
     })
 }
 
-pub fn read_storage_info() -> Result<Storage, String> {
+pub fn read_storage_info() -> Result<Storage, ReaderError> {
     let sensors = Sensor::storage_sensors();
     let disks = Disks::new_with_refreshed_list();
     let target = Path::new("/");
     let disk = disks
         .iter()
         .find(|v| target.starts_with(v.mount_point()))
-        .ok_or("NVME_AVAILABLE_SIZE sensor not found!")?;
+        .ok_or(ReaderError::SensorNotFound(format!("NVME_AVAILABLE_SIZE")))?;
 
     let raw_storage_temperature = Sensor::find_sensor(sensors, "NVME_TEMPERATURE")?.read_sensor()?;
-
     let raw_storage_size = Sensor::find_sensor(sensors, "NVME_SIZE")?.read_sensor()?;
 
     Ok(Storage {
@@ -119,3 +97,4 @@ pub fn read_storage_info() -> Result<Storage, String> {
         total: (raw_storage_size * 512) as f64 / (1024.0 * 1024.0 * 1024.0),
     })
 }
+
